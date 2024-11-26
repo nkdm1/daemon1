5e6eab4660da886940dfbe15575740708228f59c
nikodem <nikodem@konieczny.work>
2024-11-26 21:39:34 +0100

    v0.2

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # daemon1
-daemon1 is simple background process that changes the default behaviour of macos windows management and makes it a little more intuitive (imo)
+daemon1 is a background process that changes the default behaviour of macos windows management and makes it a little more intuitive (imo)
 
-## features
-*  daemon1 automatically opens a new window if switched-to application has zero windows, making you forget about command+tab+option-command shortcut
-*  if switched-to application has all minimized windows, daemon1 unminimizes the last minimized one
-
+## main features
+* automatically opens a new window if switched-to application has zero windows, no need for command+tab+option-command shortcut
+* if switched-to application has all minimized windows, daemon1 unminimizes the last minimized one
 * closing/minimizing the last window of an application hides it (performs command+tab)
 
 ## installation
@@ -12,84 +11,76 @@ daemon1 is simple background process that changes the default behaviour of macos
 daemon1 is compatible with apple m-series chips on macos 10.15 or higher
 
 1. download daemon1 executable and .plist file [here](https://github.com/nkdm1/daemon1/releases/tag/v0.1)
-2. move daemon1 to "/Applications" directory
-3. open terminal and type `cd /Applications` and then `chmod 755 "daemon1"`
-4. paste `/Applications/daemon1` into .plist file under "ProgramArguments" -> "Item 0"
+2. move daemon1 executable to "/Applications" directory
+3. open terminal and paste `cd /Applications; chmod 755 "daemon1"`
+4. open .plist file and paste `/Applications/daemon1` under "ProgramArguments" -> "Item 0"
 5. move .plist to "~/Library/LaunchAgents"
-6. open terminal and type `launchctl load ~/Library/LaunchAgents/daemon1.plist`
+6. paste `launchctl load ~/Library/LaunchAgents/daemon1.plist` into terminal
 7. ignore prompt "daemon1 not opened", don't close it
 8. go to system settings -> privacy&security -> scroll down and click "open/allow anyway" 
 9. click "open" when prompted (again) that the application couldn't be verified 
 10. you will be prompted to grant accessibility privilege, grant them in system settings -> privacy&security -> accessibility
-11. there is a possibility that you will get "There is no application set to open the document 'coreautha.bundle'." error. ignore it :)
+11. there is a possibility that you will get "There is no application set to open the document 'coreautha.bundle'." error -> ignore it 
 
 `brew install daemon1` - hopefully coming soon 
 
 ### troubleshooting
-open your activity monitor, go to memory tab and type "daemon1" in right upper corner
+open your activity monitor, go to memory tab and type "daemon1" in the right upper corner
  
-if you see daemon1 but it doesn't work, you are doomed :0 (check for a typo in .plist)
+if you see daemon1 but it doesn't work, you are doomed (uninstall and install again)
 
 if you don't see daemon1:
 
 ###### check for daemon1 in activity monitor after completing every step, you likely won't have to  go through all of them
 
-1. type `launchctl start nkdm1.daemon1` to the terminal
+1. type `launchctl start nkdm1.daemon1` in the terminal
 2. restart your computer
-3. unload with `launchctl unload ~/Library/LaunchAgents` and load it back with `launchctl load ~/Library/LaunchAgents`
-4. change your daemon1 executable directory, update your .plist, unload with `launchctl unload ~/Library/LaunchAgents` and load back with `launchctl load ~/Library/LaunchAgents`, then start with `launchctl start nkdm1.daemon1`
+3. unload with `launchctl unload ~/Library/LaunchAgents/daemon1.plist` and load it back with `launchctl load ~/Library/LaunchAgents/daemon1.plist`
+4. change your daemon1 executable directory, update your .plist, unload with `launchctl unload ~/Library/LaunchAgents/daemon1.plist` and load back with `launchctl load ~/Library/LaunchAgents/daemon1.plist`, then start with `launchctl start nkdm1.daemon1`
 5. if nothing works, please make a new [issue](https://github.com/nkdm1/daemon1/issues)
 
-
+## uninstallation
+1. paste this into the terminal:
+```bash
+launchctl unload ~/Library/LaunchAgents/daemon1.plist
+rm ~/Library/LaunchAgents/daemon1.plist
+rm /Applications/daemon1
+```
+2. remove daemon1 from system settings -> privacy&security -> accessibility
 ## usage
 daemon1 is working as a background process called "agent", which is automatically
     launched when you log in to your account
 
-it incoureges you to think a little diffrent when managing your windows
+## why was daemon1 written?
+daemon1 encourages you to think a little differently when managing your windows
 
 first of all, it is mainly written to meet my own requirements, which are:
 
 1. speed up command+tab application switching
 2. make command+w and command+m less irritating to use
 3. forget about command+h and command+tab+option-command
-4. make not quitting all of your application a good habit
+4. make not quitting all of your applications a good habit
 
-having mail, notes, safari, terminal and spotify opened in the background is important to me, because i use them all the time
+for those interested in the thinking method that guides me through this project::
 
-i don't want to launch them everytime i want to use them again, that's why i don't quit them
+- there are some applications that i don't want to quit, because they're used often [mail for example]
+- when i'm done with that application i close its windows, not the whole application
+- i want the application to be in the default state the next time i switch to it, not the state i stopped using it
+- that’s where command+h fails to do its job - it is good when you want the app to only temporarily hide and you will be switching back to it soon, with a purpose to continue your work - which is exactly what command+tab does
+- closing the last window of an application and then hiding it/switching to another application is what you want to do when you are done with the application, but at this point you have to use two shortcuts to perform one activity and you are met with another problem - switching back to that application doesn't automatically open a default window
 
-i'm a fan of "i'm done" kind of thinking - i use the application untill my mind is hit with "ok, i'm done", then i close it
+this project has been started to address this issues and hopefully make your macos life easier
 
-i want the application to be at default state the next time i switch to it, not the state i stopped using it
-
-that's where command+h fails to do it's job - it is good when you want the app to only temporarily hide and you will be switching back to it soon, with a purpose to continue your work - which is exactly what command+tab does :)
-
-closing the last window of application and then hiding it/switching to other application is what you want to do when you are done with application, but at this point you have to use two shortcuts to perform one activity and you are met with another problem - switching back to that application doesn't automatically open a default window
-
-  
-
-
-
-
-
-if you don't want to use daemon1 anymore: 
-1. delete .plist file from your ~/Library/LaunchAgents/ directory
-2. type `launchctl unload ~/Library/LaunchAgents` into the terminal
-3. remove daemon1 executable from your chosen directory 
-    
 ## contribution 
-pull requests are more than welcome
-
-for major changes, please open an [issue](https://github.com/nkdm1/daemon1/issues) first to discuss what you would like to change
+any type of pull requests are more than welcome
 
 ## known problems 
-when an apppication has some windows minimized and you close the last not-minimized window (which triggers app-switch), switching back to that 
-    application unminiaturizes the oldest window, not the last minimized one
-
-i made it unminiaturize all windows, instead of the oldest, until i fix 
+1. when an apppication has some windows minimized and you close the last not-minimized window (which triggers app-switch), switching back to that 
+    application unminiaturizes the oldest window, not the last minimized one. i made it unminiaturize all windows, instead of the oldest, until i fix 
+2. from time to time hiding the last application does bring the finder window to the front for some reason
 
 ## license
-this project is under [gnu gplv3](https://www.gnu.org/licenses/gpl-3.0.en.html#license-text)  license
+this project is under [gnu gplv3](https://www.gnu.org/licenses/gpl-3.0.en.html#license-text) license
 
 ## credits
 big thanks to every contributor of [swindler](https://github.com/tmandry/Swindler), this project was fucking annoying to write until i found your work guys
--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -9,6 +9,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         guard AXSwift.checkIsProcessTrusted(prompt: true) else {
             print("Not trusted as an AX process; please authorize and re-launch")
+            sleep(10)
             NSApp.terminate(self)
             return
         }
@@ -16,6 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         Swindler.initialize().done { state in
             self.swindler = state
             self.setupEventHandlers()
+            
         }.catch { error in
             print("Fatal error: failed to initialize Swindler: \(error)")
             NSApp.terminate(self)
@@ -23,66 +25,202 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     private func setupEventHandlers() {
-        let ignoredApplications: [String] = ["Stats"]
+        let ignoredApplicationsList = self.setupIgnoredApplications()
         
-        
-        swindler.on { (event: WindowCreatedEvent) in
-            //window creation logic
-        }
-        swindler.on { (event: WindowFrameChangedEvent) in
-           // window movement logic
-        }
         swindler.on { (event: WindowDestroyedEvent) in
-            if event.window.application.bundleIdentifier == self.swindler.frontmostApplication.value?.bundleIdentifier {
-                print("window destroyed: \(event.window.title.value)")
-                let windows = self.swindler.frontmostApplication.value?.knownWindows
-                var minimizedWindowsCount = 0
-                for window in windows ?? [] {
-                    if window.isMinimized.value {
-                        minimizedWindowsCount += 1
-                    }
-                }
-                if minimizedWindowsCount == windows?.count {
-                    print("last window closed")
-                    event.window.application.isHidden.value = true
-                } else{
-                    print("app still have existing windows")
-                }
-            }
-        }
-        swindler.on { (event: ApplicationMainWindowChangedEvent) in
-            // app main window changed logic
-        }
-        swindler.on { (event: FrontmostApplicationChangedEvent) in
-            let newFrontmostApp = String(event.newValue?.bundleIdentifier?.split(separator: ".").last ?? "unknown")
-            let oldFrontmostapp = String(event.oldValue?.bundleIdentifier?.split(separator: ".").last ?? "unknown")
-            
-            guard !ignoredApplications.contains(newFrontmostApp) && !ignoredApplications.contains(oldFrontmostapp) else {
+            guard !ignoredApplicationsList.contains(event.window.application.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown") else {
                 return
             }
+            guard event.window.application.bundleIdentifier == self.swindler.frontmostApplication.value?.bundleIdentifier && NSWorkspace.shared.menuBarOwningApplication?.bundleIdentifier == event.window.application.bundleIdentifier else {
+                return
+            }
+            
+            let finder = self.swindler.runningApplications.first(where: {$0.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown" == "finder"})
+            let allApplications = self.swindler.runningApplications
+            var minimizedApplicationWindowsCount = 0
+            var visibleOtherWindowsCount = 0
+            var hiddenOtherApplicationsCount = 0
+            print("window destroyed: \(event.window.title.value)")
+            
+            for app in allApplications {
+                if app == event.window.application {
+                    for window in app.knownWindows{
+                        if window.isMinimized.value {
+                            minimizedApplicationWindowsCount += 1
+                        }
+                    }
+                }
+                else {
+                    for window in app.knownWindows{
+                        if !window.isMinimized.value && !app.isHidden.value{
+                            visibleOtherWindowsCount += 1
+                        }
+                    }
+                }
+            }
+            if minimizedApplicationWindowsCount == event.window.application.knownWindows.count {
+                if visibleOtherWindowsCount != 0 {
+                    print("last window closed")
+                    event.window.application.isHidden.value = true
+                }
+                else {
+                    print("app still have existing windows or it was the last visible window")
+                    // switch to finder without opening the window
+                    if let finder {
+                        self.swindler.frontmostApplication.value = finder
+                    }
+                }
+            }
+        }
+        
+        swindler.on { (event: FrontmostApplicationChangedEvent) in
+
+            /*
+            guard !ignoredApplicationsList.contains(newFrontmostApp) && !ignoredApplicationsList.contains(oldFrontmostapp) else {
+                return
+            }
+             */
+            guard event.newValue?.bundleIdentifier == self.swindler.frontmostApplication.value?.bundleIdentifier && NSWorkspace.shared.menuBarOwningApplication?.bundleIdentifier == event.newValue?.bundleIdentifier && event.external == true else {
+                return
+            }
+            let finder = self.getFinder()
+            let newFrontmostApp = String(event.newValue?.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown")
+            let oldFrontmostapp = String(event.oldValue?.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown")
+            if self.swindler.frontmostApplication.value == finder {
+                print("finder is frontmost")
+                print(event.newValue?.knownWindows ?? "brak okien")
+                print(event.newValue?.knownWindows.count ?? "0")
+            }
             print("new frontmost app: \(newFrontmostApp).",
-                  "[old: \(event.oldValue?.bundleIdentifier?.split(separator: ".").last ?? "unknown")]")
+                  "[old: \(oldFrontmostapp)]")
             
             self.frontmostApplicationChanged()
         }
+        
         swindler.on { (event: WindowMinimizedChangedEvent) in
+            guard !ignoredApplicationsList.contains(event.window.application.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown")
+            // && event.newValue.description.contains("true") -> sprawdzić czy działa tylko gdy minimalizujemy
+            else {
+                return
+            }
+            
+            let finder = self.getFinder()
+            let allApplications = self.swindler.runningApplications
+            var minimizedApplicationWindowsCount = 0
+            var visibleOtherWindowsCount = 0
+            var hiddenOtherApplicationsCount = 0
             print("window (un)minimized: \(event.window.title.value)")
-            var minimizedWindowsCount = 0
-            let windows: [Window]? = self.swindler.frontmostApplication.value?.knownWindows
-            for window in windows ?? [] {
-                if window.isMinimized.value {
-                    minimizedWindowsCount += 1
+            
+            
+            for app in allApplications {
+                if app == event.window.application {
+                    for window in app.knownWindows{
+                        if window.isMinimized.value {
+                            minimizedApplicationWindowsCount += 1
+                        }
+                    }
+                }
+                else {
+                    for window in app.knownWindows{
+                        if !window.isMinimized.value && !app.isHidden.value {
+                            visibleOtherWindowsCount += 1
+                        }
+                    }
                 }
             }
-            if windows?.count == minimizedWindowsCount {
-                self.swindler.frontmostApplication.value?.isHidden.value = true
+            
+            if minimizedApplicationWindowsCount == event.window.application.knownWindows.count {
+                if visibleOtherWindowsCount != 0 {
+                    print("last window closed")
+                    event.window.application.isHidden.value = true
+                }
+                else {
+                    print("app still have existing windows or it was the last visible window")
+                    // switch to finder without opening the window
+                    if let finder {
+                        self.swindler.frontmostApplication.value = finder
+                    }
+                }
             }
         }
+        
+        swindler.on {(event: ApplicationIsHiddenChangedEvent) in // it sometimes does sometimes doesn't bring finder window to front i dont know why
+            let finder = self.getFinder()
+            guard event.external == true && self.swindler.frontmostApplication.value != finder ?? .none else {
+                return
+            }
+            
+            var noWindowVisible = true
+            print(event.newValue.description)
+            for app in self.swindler.runningApplications {
+                var shouldBrake = false
+                for window in app.knownWindows {
+                    if !window.isMinimized.value && !app.isHidden.value {
+                        noWindowVisible = false
+                        shouldBrake = true
+                        break
+                    }
+                    if shouldBrake {
+                        break
+                    }
+                }
+            }
+            if noWindowVisible {
+                if let finder {
+                    self.swindler.frontmostApplication.value = finder
+                }
+            }
+            
+            
+            
+            
+        }
+        
+        /*
+        swindler.on { (event: WindowCreatedEvent) in
+            event.window.application.isHidden.value = false
+        }
+        
+        swindler.on { (event: ApplicationMainWindowChangedEvent) in
+            // app main window changed logic
+        }
+        
+        swindler.on { (event: WindowFrameChangedEvent) in
+           // window movement logic
+        }
+        */
+    }
+    
+    private func setupIgnoredApplications() -> [String] {
+        var ignoredApplicationsList: [String] = []
+        
+        if !FileManager.default.fileExists(atPath: NSHomeDirectory() + "/Library/daemon1") {
+            try? FileManager.default.createDirectory(atPath: NSHomeDirectory() + "/Library/daemon1", withIntermediateDirectories: true)
+        }
+        
+        let ignoredApplicationsFilePath = NSHomeDirectory() + "/Library/daemon1/ignoredapplications.txt"
+        
+        if FileManager.default.fileExists(atPath: ignoredApplicationsFilePath) {
+            let readApplications = try? String(contentsOfFile: ignoredApplicationsFilePath).split(separator: "\n")
+            for application in readApplications ?? [] {
+                ignoredApplicationsList.append(String(application).lowercased())
+            }
+        }
+        
+        return ignoredApplicationsList  
+    }
+    private func hideApplication(_ app: Swindler.Application)
+    {
+        app.isHidden.value = true
+    }
+    
+    func getFinder() -> Swindler.Application?{
+        return swindler.runningApplications.first(where: {$0.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown" == "finder"})
     }
     
     private func frontmostApplicationChanged() {
         let windows: [Window]? = swindler.frontmostApplication.value?.knownWindows
-        if windows?.count == 0{
+        if windows?.count == 0 {
             if let URL = NSWorkspace.shared.frontmostApplication?.bundleURL {
                 NSWorkspace.shared.openApplication(at: URL, configuration: OpenConfig())
             }
@@ -113,9 +251,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 }
 
+
+
 func openApplicationByNSWorkspace(_ url: URL){
     let OpenConfig = OpenConfig()
     NSWorkspace.shared.openApplication(at: url, configuration: OpenConfig)
+}
+
+func dispatchAfter(delay: TimeInterval, block: DispatchWorkItem) {
+    let time = DispatchTime.now() + delay
+    DispatchQueue.main.asyncAfter(deadline: time, execute: block)
 }
 
 class OpenConfig: NSWorkspace.OpenConfiguration{
