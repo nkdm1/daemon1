ad9b2b78af4860c82d0be05f40ba29ebfd6875a8
nikodem <nikodem@konieczny.work>
2024-12-18 11:35:47 +0100

    lots of changes, mostly bug fixes and improvements to existing features

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -1,10 +1,16 @@
 import AppKit
 import AXSwift
 import Swindler
+import Quartz
+import Cocoa
 
+ 
 
+@MainActor
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let VC = ViewController()
     var swindler: Swindler.State!
+    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         guard AXSwift.checkIsProcessTrusted(prompt: true) else {
@@ -26,15 +32,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     private func setupEventHandlers() {
         let ignoredApplicationsList = self.setupIgnoredApplications()
+        let delayedApplicationsList = self.setupDelayedApplications()
         
         swindler.on { (event: WindowDestroyedEvent) in
-            guard !ignoredApplicationsList.contains(event.window.application.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown") else {
+            guard !ignoredApplicationsList.contains(event.window.application.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown") && self.swindler.runningApplications.contains(event.window.application) else {
                 return
             }
-            guard event.window.application.bundleIdentifier == self.swindler.frontmostApplication.value?.bundleIdentifier && NSWorkspace.shared.menuBarOwningApplication?.bundleIdentifier == event.window.application.bundleIdentifier else {
-                return
-            }
-            
+//            guard event.window.application.bundleIdentifier == self.swindler.frontmostApplication.value?.bundleIdentifier && NSWorkspace.shared.menuBarOwningApplication?.bundleIdentifier == event.window.application.bundleIdentifier else {
+//                return
+//            }
+            let lastCommandWTimestamp = self.VC.lastCommandWTimestamp
             let finder = self.swindler.runningApplications.first(where: {$0.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown" == "finder"})
             let allApplications = self.swindler.runningApplications
             var minimizedApplicationWindowsCount = 0
@@ -57,37 +64,74 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     }
                 }
             }
-            if minimizedApplicationWindowsCount == event.window.application.knownWindows.count {
-                if visibleOtherWindowsCount != 0 {
-                    print("last window closed")
-                    event.window.application.isHidden.value = true
-                }
-                else {
-                    print("app still have existing windows or it was the last visible window")
-                    // switch to finder without opening the window
-                    if let finder {
-                        self.swindler.frontmostApplication.value = finder
+            
+            if Date().timeIntervalSinceReferenceDate - lastCommandWTimestamp > 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)){
+                    if minimizedApplicationWindowsCount == event.window.application.knownWindows.count {
+                        if visibleOtherWindowsCount != 0 {
+                            print("last window closed")
+                            event.window.application.isHidden.value = true
+                        }
+                        else {
+                            print("app still have existing windows or it was the last visible window")
+                            if let finder {
+                                self.swindler.frontmostApplication.value = finder
+                            }
+                        }
                     }
                 }
             }
+            else {
+                if minimizedApplicationWindowsCount == event.window.application.knownWindows.count {
+                    if visibleOtherWindowsCount != 0 {
+                        print("last window closed")
+                        event.window.application.isHidden.value = true
+                    }
+                    else {
+                        print("app still have existing windows or it was the last visible window")
+                        if let finder {
+                            self.swindler.frontmostApplication.value = finder
+                        }
+                    }
+                }
+            }
+            
         }
         
         swindler.on { (event: FrontmostApplicationChangedEvent) in
-
+            let finder = self.getFinder()
             let newFrontmostApp = String(event.newValue?.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown")
-            let oldFrontmostapp = String(event.oldValue?.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown")
+            let oldFrontmostApp = String(event.oldValue?.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown")
             
-            guard !ignoredApplicationsList.contains(newFrontmostApp) && !ignoredApplicationsList.contains(oldFrontmostapp) else {
+            guard !ignoredApplicationsList.contains(newFrontmostApp) && !ignoredApplicationsList.contains(oldFrontmostApp) else {
                 return
             }
             
             guard event.newValue?.bundleIdentifier == self.swindler.frontmostApplication.value?.bundleIdentifier && NSWorkspace.shared.menuBarOwningApplication?.bundleIdentifier == event.newValue?.bundleIdentifier && event.external == true else {
                 return
             }
-            print("new frontmost app: \(newFrontmostApp).",
-                  "[old: \(oldFrontmostapp)]")
+            if event.newValue == finder {
+                DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(25)){
+                    let noWindowsVisible: Bool = self.noWindowsAreVisible()
+                    if noWindowsVisible{
+                        return
+                    }
+                    print("new frontmost app: \(newFrontmostApp).",
+                          "[old: \(oldFrontmostApp)]")
+                    
+                    self.frontmostApplicationChanged()
+                }
+            }
+            else {
+                print("new frontmost app: \(newFrontmostApp).",
+                      "[old: \(oldFrontmostApp)]")
+                
+                self.frontmostApplicationChanged()
+            }
             
-            self.frontmostApplicationChanged()
+            
+            
+            
         }
         
         swindler.on { (event: WindowMinimizedChangedEvent) in
@@ -136,51 +180,102 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
         
-        swindler.on {(event: ApplicationIsHiddenChangedEvent) in // it sometimes does sometimes doesn't bring finder window to front i dont know why
-            let finder = self.getFinder()
-            guard event.external == true && self.swindler.frontmostApplication.value != finder ?? .none && event.newValue.description.contains("true") else {
-                return
-            }
+//        swindler.on {(event: ApplicationIsHiddenChangedEvent) in // it sometimes does sometimes doesn't bring finder window to front i dont know why
+//            let finder = self.getFinder()
+//            guard event.external == true && self.swindler.frontmostApplication.value != finder ?? .none && event.newValue.description.contains("true") else {
+//                return
+//            }
+//            
+//            var noWindowVisible = true
+//            print(event.newValue.description)
+//            for app in self.swindler.runningApplications {
+//                var shouldBrake = false
+//                for window in app.knownWindows {
+//                    if !window.isMinimized.value || !app.isHidden.value {
+//                        noWindowVisible = false
+//                        shouldBrake = true
+//                        break
+//                    }
+//                }
+//                if shouldBrake {
+//                    break
+//                }
+//            }
+//            if noWindowVisible {
+//                if let finder {
+//                    self.swindler.frontmostApplication.value = finder
+//                }
+//            }
+//        }
+//        swindler.on { (event: ApplicationTerminatedEvent) in
+//            guard !ignoredApplicationsList.contains(event.application.bundleIdentifier?.split(separator: ".").last?.lowercased() ?? "unknown")
+//            else {return}
+//            
+//            let finder = self.getFinder()
+//            var noWindowVisible = true
+//            for app in self.swindler.runningApplications {
+//                var shouldBrake = false
+//                for window in app.knownWindows {
+//                    if !window.isMinimized.value || !app.isHidden.value {
+//                        noWindowVisible = false
+//                        shouldBrake = true
+//                        break
+//                    }
+//                }
+//                if shouldBrake {
+//                    break
+//                }
+//            }
+//            if noWindowVisible {
+//                if let finder {
+//                    self.swindler.frontmostApplication.value = finder
+//                }
+//            }
+//            
+//        }
             
-            var noWindowVisible = true
-            print(event.newValue.description)
-            for app in self.swindler.runningApplications {
-                var shouldBrake = false
-                for window in app.knownWindows {
-                    if !window.isMinimized.value && !app.isHidden.value {
-                        noWindowVisible = false
-                        shouldBrake = true
-                        break
-                    }
-                    if shouldBrake {
-                        break
-                    }
-                }
-            }
-            if noWindowVisible {
-                if let finder {
-                    self.swindler.frontmostApplication.value = finder
-                }
-            }
+                
             
-            
-            
-            
-        }
+        
         
         /*
         swindler.on { (event: WindowCreatedEvent) in
             event.window.application.isHidden.value = false
         }
-        
-        swindler.on { (event: ApplicationMainWindowChangedEvent) in
-            // app main window changed logic
-        }
-        
+        */
+//        swindler.on { (event: ApplicationMainWindowChangedEvent) in
+//            self.swindler.frontmostApplication.value = event.application
+//            event.application.isHidden.value = false
+//        }
+        /*
         swindler.on { (event: WindowFrameChangedEvent) in
            // window movement logic
         }
         */
+    }
+    
+    
+    private func noWindowsAreVisible() -> Bool {
+        let finder = getFinder()
+        var noWindowVisible = true
+        for app in self.swindler.runningApplications {
+            
+            if app.isHidden.value {
+                continue
+            }
+            var shouldBrake = false
+            for window in app.knownWindows {
+                if !window.isMinimized.value {
+                    noWindowVisible = false
+                    shouldBrake = true
+                    break
+                }
+            }
+            if shouldBrake {
+                break
+            }
+        }
+        return noWindowVisible
     }
     
     private func setupIgnoredApplications() -> [String] {
@@ -200,6 +295,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         
         return ignoredApplicationsList  
+    }
+    private func setupDelayedApplications() -> [String] {
+        var ignoredApplicationsList: [String] = ["stats","textedit","preview","word","excel","powerpoint"]
+        
+        if !FileManager.default.fileExists(atPath: NSHomeDirectory() + "/Library/daemon1") {
+            try? FileManager.default.createDirectory(atPath: NSHomeDirectory() + "/Library/daemon1", withIntermediateDirectories: true)
+        }
+        
+        let delayed = NSHomeDirectory() + "/Library/daemon1/delayed.txt"
+        
+        if FileManager.default.fileExists(atPath: delayed) {
+            let readApplications = try? String(contentsOfFile: delayed).split(separator: "\n")
+            for application in readApplications ?? [] {
+                ignoredApplicationsList.append(String(application).lowercased())
+            }
+        }
+        
+        return ignoredApplicationsList
     }
     private func hideApplication(_ app: Swindler.Application)
     {
@@ -241,9 +354,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
+    
+    
+}
+
+@MainActor
+class ViewController: NSViewController {
+    public var lastCommandWTimestamp: Double = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSEvent.addGlobalMonitorForEvents(matching: [.keyDown, .flagsChanged]) { (event) in
+            if event.keyCode == 13 && event.modifierFlags.intersection(.deviceIndependentFlagsMask) == [.command] {
+                print("kurwa działa", Date().timeIntervalSinceReferenceDate)
+                self.lastCommandWTimestamp = Date().timeIntervalSinceReferenceDate
+            }
+        }
+    }
 }
 
 
+func lastCommandWDetection() -> Date?{
+    return Date()
+}
 
 func openApplicationByNSWorkspace(_ url: URL){
     let OpenConfig = OpenConfig()
@@ -268,6 +400,7 @@ class OpenConfig: NSWorkspace.OpenConfiguration{
         hidesOthers = false
     }
 }
+
 
 
 
--- Sources/daemon1.swift
+++ Sources/daemon1.swift
@@ -5,9 +5,16 @@ import Swindler
 struct daemon1 {
     static func main() {
         let applicationDelegate = AppDelegate()
-        let application = NSApplication.shared  
+        let application = NSApplication.shared
+        applicationDelegate.VC.viewDidLoad()
         application.setActivationPolicy(NSApplication.ActivationPolicy.accessory)
         application.delegate = applicationDelegate
         application.run()
+        
+        // dodac observer command+w
+        // i przy zamknieciu okna sprawdzac czy command+w nie zostalo klikniete w przeciagu idk 0.5s
+        // jesli nie, to dopiero po dispatchqueue.delay, 0.5s - 1s sprawdzac czy okna istnieja
+        
+        
     }
 }
